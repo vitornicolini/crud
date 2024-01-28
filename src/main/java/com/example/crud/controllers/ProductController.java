b35616c1069e8279e06b0bfe784c84ce529feb02
vitornicolini <vitornicolini@gmail.com>
2024-01-28 09:47:48 -0300

    initial project

---
+++ src/main/java/com/example/crud/controllers/ProductController.java
@@ -0,0 +1,15 @@
+package com.example.crud.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+    @GetMapping
+    public ResponseEntity getAllProducts(){
+        return ResponseEntity.ok("deu ok");
+    }
+}
